618614f5b8b3dae6f36efb922a01e448d674b5cf
Shoham Shitrit Shoham Shitrit <Shoham Shitrit>
2020-03-12 16:39:44 -0400

    got some stuff donwe

--- A2.lisp
+++ A2.lisp
@@ -11,7 +11,7 @@
        (cond ((atom wff) wff)
              (t (cons (cons '- (cdr wff))
                       (mapcar #'(lambda (x) (cons (car wff) x))
-                          (-keys (cdr wff)))))))
+                          (dash-keys (cdr wff)))))))
 
 
 
@@ -24,30 +24,39 @@
    
 )
 
-(defun add-list-to-kb (l value)
+(defun add-list-to-kb (list. value)
+    (setq l (car list.))
     (setq index (gethash l *KB*))
+    ;(print l)
+    ;(print value)
+    (setq myset '()) ;;create new set and add valueu
+    (setq myset (adjoin l myset))
     (cond
-        ((eq index nil));if we do not hve a value in place for this list
-        (
-            (setq myset '()) ;;create new set and add valueu
-            (adjoin l myset)
-            (setf (gethash l *KB*) myset)
+        ((eq index nil);if we do not hve a value in place for this list
+        
+            (format t "No index")
+            (setf (gethash l *KB*) value)
         )
-        ((not (eq index nil))) 
+        ((not (eq index nil)) 
         (
-            (if (not (member index value))
-                (setf (gethash l *KB*) (adjoin index value))
-            )
+            ;(if (not (member index value))
+            ;    (setf (gethash l *KB*) (adjoin index value))
+            ;    nil
+            ;)
+            (format t "Got here")
         )
+        )
+        (t (format t "Fuclk"))
     )
+    (add-to-kb (cdr list.) value)
 )
 
 (defun add-to-kb (all-combos value)
+  ;  (print all-combos)
     (if (eq all-combos nil)
         nil
-        (add-list-to-kb (car all-combos) value)
+        (add-list-to-kb all-combos value)
     )
-    (add-to-kb (cdr all-combos) value)
 )
 
 (defun store-longer-fact (wff)
@@ -60,7 +69,8 @@
 )
 
 ;(store-fact '(A B C))
+;(print(dash-keys '(Robot Robbie)))
 (store-fact '(Robot Robbie))
 (loop for k being each hash-key of *KB*
-	 do (print k)
+	 do (format t "~%~a ~a~%" k (gethash k *KB*))
 )
